14026638611a3b6933dfe4c19c0e765f0d7578de
kosskas <93159483+kosskas@users.noreply.github.com>
2023-12-29 15:49:11 +0100

    Mod VL script

--- Assets/Scripts/VertexLabels.cs
+++ Assets/Scripts/VertexLabels.cs
@@ -2,37 +2,50 @@
 
 public class VertexLabels : MonoBehaviour
 {
-    public float labelOffset = 0.1f; // Odległość etykiety nad wierzchołkiem
-    public float sideOffset = 0.1f; // Dodatkowa odległość w bok
+    public float labelOffset = 0.1f;
+    public float sideOffset = 0.1f;
+    Vector3[] vertices;
+    GameObject[] labels;
 
     void Start()
     {
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshFilter meshFilter = GetComponentInChildren<MeshFilter>();
 
         if (meshFilter != null)
         {
-            Mesh mesh = meshFilter.mesh;
-            Vector3[] vertices = mesh.vertices;
+            vertices = meshFilter.mesh.vertices;
+            labels = new GameObject[vertices.Length / 3];
 
-            for (int i = 0; i < vertices.Length; i++)
+            for (int i = 0; i < vertices.Length / 3; i++)
             {
-                // Tworzenie obiektu tekstu nad wierzchołkiem
                 GameObject label = new GameObject("VertexLabel" + i);
                 TextMesh textMesh = label.AddComponent<TextMesh>();
                 textMesh.text = ((char)('A' + i)).ToString();
-                textMesh.anchor = TextAnchor.MiddleCenter;
-                textMesh.alignment = TextAlignment.Center;
-                textMesh.characterSize = 0.2f; // Dostosuj rozmiar litery
-
-                // Ustawianie pozycji tekstu nad wierzchołkiem z uwzględnieniem odległości w bok
-                label.transform.position = transform.TransformPoint(vertices[i]) + Vector3.up * labelOffset + Vector3.right * sideOffset;
-                label.transform.rotation = Quaternion.identity;
-                label.transform.parent = transform;
+                textMesh.characterSize = 0.1f;
+                textMesh.color = Color.black;
+                labels[i] = label;
             }
         }
         else
         {
-            Debug.LogError("Brakuje komponentu MeshFilter!");
+            Debug.LogError("MeshFilter component not found!");
         }
     }
+
+    void Update()
+    {
+        for (int i = 0; i < labels.Length; i++)
+        {
+            Vector3 localOffset = Vector3.up * labelOffset + Vector3.right * sideOffset;
+            Vector3 worldPosition = transform.TransformPoint(vertices[i]) + transform.rotation * localOffset;
+            labels[i].transform.position = worldPosition;
+
+            // Use the object's rotation directly
+            labels[i].transform.rotation = transform.rotation;
+
+            //labels[i].transform.parent = transform;
+        }
+    }
+
 }
+
