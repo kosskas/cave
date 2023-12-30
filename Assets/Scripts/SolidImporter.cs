d36574afeed4b9f7853e48b9dfe12853e5b3887c
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2023-12-30 04:35:23 +0100

    Add picking a solid to show

--- Assets/Scripts/SolidImporter.cs
+++ Assets/Scripts/SolidImporter.cs
@@ -11,23 +11,30 @@ public class SolidImporter : MonoBehaviour {
 	private const string solidFileExt = "*.wobj";
 	
 	private string[] solidFiles;
+	private int currentSolidFileIndex;
 
 	// Use this for initialization
 	void Start () {
 		solidFiles = Directory.GetFiles(pathToFolderWithSolids, solidFileExt);
+		currentSolidFileIndex = 0;
 
 		LogStatus();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if(Input.GetKeyDown("p"))
+		{
+			PickNextSolid();
+			LogStatus();
+		}
 	}
 
 	private void LogStatus() {
 		if (solidFiles.Length > 0)
 		{
 			StringBuilder infoString = new StringBuilder();
+			infoString.Append(String.Format(" Current solid: {0}", Path.GetFileName(solidFiles[currentSolidFileIndex])));
 			infoString.Append(String.Format("\n | All solids:"));
 			foreach (string solidFile in solidFiles)
 			{
@@ -39,5 +46,9 @@ public class SolidImporter : MonoBehaviour {
 		{
 			Debug.LogError("No solids found!");
 		}
+	}
+
+	private void PickNextSolid() {
+		currentSolidFileIndex = (currentSolidFileIndex + 1) % solidFiles.Length;
 	}
 }
