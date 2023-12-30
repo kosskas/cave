523a8f998e493282d4468d92b28eac4ac77b39b0
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2023-12-30 04:32:56 +0100

    Add .wobj files finding

---
+++ Assets/Scripts/SolidImporter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+
+public class SolidImporter : MonoBehaviour {
+
+	private const string pathToFolderWithSolids = "./Assets/Figures3D";
+	private const string solidFileExt = "*.wobj";
+	
+	private string[] solidFiles;
+
+	// Use this for initialization
+	void Start () {
+		solidFiles = Directory.GetFiles(pathToFolderWithSolids, solidFileExt);
+
+		LogStatus();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void LogStatus() {
+		if (solidFiles.Length > 0)
+		{
+			StringBuilder infoString = new StringBuilder();
+			infoString.Append(String.Format("\n | All solids:"));
+			foreach (string solidFile in solidFiles)
+			{
+				infoString.Append(String.Format(" {0}", Path.GetFileName(solidFile)));
+			}
+			Debug.Log(infoString.ToString());
+		}
+		else
+		{
+			Debug.LogError("No solids found!");
+		}
+	}
+}
