e6b486bc26bf312396eef430f4b08088a3c81409
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-02 11:02:29 +0100

    Add ClearLabels func

--- Assets/Scripts/VertexLabels.cs
+++ Assets/Scripts/VertexLabels.cs
@@ -29,19 +29,22 @@ public class VertexLabels : MonoBehaviour
         {
             InitLabels(null);
         }
-        //Parametry obrotu dziecka MainObject czyli właściwego obiektu
-        //pod 0 musi być właściwy obiekt
-        Quaternion rotation = transform.GetChild(0).rotation;
-        
-        for (int i = 0; i < labels.Length; i++)
+        if (transform.childCount > 0)
         {
-            Vector3 rotatedVertex = rotation * vertices[i];
-            Vector3 worldPosition = transform.TransformPoint(rotatedVertex) + Vector3.up * labelOffset;
-            labels[i].transform.position = worldPosition;
-            Vector3 directionToPlayer = (player.transform.position - labels[i].transform.position).normalized;
+            //Parametry obrotu dziecka MainObject czyli właściwego obiektu
+            //pod 0 musi być właściwy obiekt
+            Quaternion rotation = transform.GetChild(0).rotation;
 
-            //literki są twarzą do maincamery
-            labels[i].transform.rotation = Quaternion.LookRotation(-directionToPlayer);
+            for (int i = 0; i < labels.Length; i++)
+            {
+                Vector3 rotatedVertex = rotation * vertices[3*i];
+                Vector3 worldPosition = transform.TransformPoint(rotatedVertex) + Vector3.up * labelOffset;
+                labels[i].transform.position = worldPosition;
+                Vector3 directionToPlayer = (player.transform.position - labels[i].transform.position).normalized;
+
+                //literki są twarzą do postaci gracza
+                labels[i].transform.rotation = Quaternion.LookRotation(-directionToPlayer);
+            }
         }
     }
 
@@ -60,25 +63,13 @@ public class VertexLabels : MonoBehaviour
         if (meshFilter != null)
         {
             vertices = meshFilter.mesh.vertices;
+            Debug.Log(vertices.Length);
             labels = new GameObject[vertices.Length / 3];
             
             if(labelsnames == null)
-            {
-                labelsnames = new string[labels.Length];
-                for(int i = 0;i < labelsnames.Length; i++)
-                {
-                    labelsnames[i] = ((char)('A' + i)).ToString();
-                }
-                //jeśli jest więcej
-                for(int i = 26; i < labelsnames.Length; i++)
-                {
-                    char first = (char)('A' + (i - 26 / 26));
-                    char second = (char)('A' +(i - 26 % 26));
-                    labelsnames[i] = $"{first}{second}";
-                }
-            }
+                labelsnames = GetBaseLabels();
 
-            for (int i = 0; i < vertices.Length / 3; i++)
+            for (int i = 0; i < labels.Length; i++)
             {
                 GameObject label = new GameObject("VertexLabel" + i);
                 TextMesh textMesh = label.AddComponent<TextMesh>();
@@ -93,6 +84,25 @@ public class VertexLabels : MonoBehaviour
         {
             Debug.LogError("Brak MeshFilter potrzebnego do działania VertexLabels.cs");
         }
+    }
+    string[] GetBaseLabels()
+    {
+        int MAXLABELSNUMBER = 50; //max liczba wierzchołków do nazwania
+        //teoretyczna max liczba dla tego algorytmu to 26+26*26 = 702 wierzchołków
+        
+        string[] labelsnames = new string[MAXLABELSNUMBER];
+        for (int i = 0; i < labelsnames.Length; i++)
+        {
+            labelsnames[i] = ((char)('A' + i)).ToString();
+        }
+        //jeśli jest więcej
+        for (int i = 26; i < labelsnames.Length; i++)
+        {
+            char first = (char)('A' + (i - 26 / 26));
+            char second = (char)('A' + (i - 26 % 26));
+            labelsnames[i] = $"{first}{second}";
+        }
+        return labelsnames;
     }
 
 }
