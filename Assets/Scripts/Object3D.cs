5ed24167fd421355d06a6b34efc5773ab8e0003b
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-07 22:02:46 +0100

    Add class template

--- Assets/Scripts/Object3D.cs
+++ Assets/Scripts/Object3D.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 /// <summary>
-/// Test klasy
+/// Centralna klasa sterująca wyświetlanym obiektem
 /// </summary>
 public class Object3D : MonoBehaviour {
 
-	/// <summary>
-	/// TETETETE
-	/// </summary>
+	//public GameObject obj;
+	//public Transform objtransform;
+	private MeshCollider meshCollider;
+
+	public void InitObject(){
+	}
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		///Tutaj wszystkie update'y ???
+		///VertexLabel.Update??
+		///EdgeLabel.Update??
+		///
 	}
 }
