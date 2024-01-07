eed725762184a82ddc506857678f71e9a9250a5b
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-07 22:59:55 +0100

    Add CreateMesh

--- Assets/Scripts/Object3D.cs
+++ Assets/Scripts/Object3D.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,22 +9,50 @@ using UnityEngineInternal;
 /// </summary>
 public class Object3D : MonoBehaviour {
 
-	//public GameObject obj;
-	//public Transform objtransform;
-	private MeshCollider meshCollider;
+	private List<Vector3> vertices = new List<Vector3>();
+	private List<int> triangles = new List<int>();
+	private Dictionary<string, Vector3> labeledVertices = new Dictionary<string, Vector3>();
+	private List<List<string>> faces = new List<List<string>>();
+	private MeshFilter meshFilter;
 
-	public void InitObject(){
+	public void InitObject(List<Vector3> vertices, List<int> triangles, Dictionary<string, Vector3> labeledVertices){
+		this.vertices = vertices;
+		this.triangles = triangles;
+		this.labeledVertices = labeledVertices;
+		CreateMesh();
+		AddVertexLabels();
 	}
 
-	void Start () {
-		
-	}
+    private void AddVertexLabels() {
+        //Dodanie oznaczeń i wyświetlanie wierzchołków
+		VertexLabels vl = gameObject.AddComponent<VertexLabels>();
+		vl.InitLabels(meshFilter, labeledVertices);	
+    }
+
+    private void CreateMesh() {
+        // Create a new mesh
+        Mesh mesh = new Mesh();
+        
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+
+        // Recalculate normals and bounds
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        // Add a MeshFilter component to the GameObject
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        // Add a MeshRenderer component to the GameObject
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        // Set the material for the MeshRenderer (you can create your own material or use an existing one)
+        meshRenderer.material = new Material(Shader.Find("Standard"));
+
+        // Assign the generated mesh to the MeshFilter
+        meshFilter.mesh = mesh;
+    }
+
 	
-	// Update is called once per frame
-	void Update () {
-		///Tutaj wszystkie update'y ???
-		///VertexLabel.Update??
-		///EdgeLabel.Update??
-		///
-	}
+
 }
