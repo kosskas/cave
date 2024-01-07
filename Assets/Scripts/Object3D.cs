74ea0b109900cbd1db35e77d9f10dd8f2dfdb9fa
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-07 21:13:34 +0100

    Add Object3D central control flow class

---
+++ Assets/Scripts/Object3D.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Test klasy
+/// </summary>
+public class Object3D : MonoBehaviour {
+
+	/// <summary>
+	/// TETETETE
+	/// </summary>
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
