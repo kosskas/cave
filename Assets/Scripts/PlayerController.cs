9b975ae66f71f64654ed8de2c67b0fcc8b09e9cc
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-18 13:59:40 +0100

    Standarize Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
-    //public float jumpSpeed = 8.0f;
-    //public float gravity = 20.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
-
+    public bool canMove = true;
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
-
     //[HideInInspector]
-    public bool canMove = true;
 
+    SolidImporter si;
+    CameraScript cs;
     void Start()
     {
         characterController = GetComponentInChildren<CharacterController>();
         playerCamera = GetComponentInChildren<Camera>();
+        si = (SolidImporter)GameObject.FindObjectOfType(typeof(SolidImporter));
+        cs = (CameraScript)GameObject.FindObjectOfType(typeof(CameraScript));
         // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -32,20 +31,29 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // We are grounded, so recalculate move direction based on axes
+        //from CameraScript
+        if(Input.GetKeyDown("1"))
+        {
+			cs.SetCam1();			
+        }
+        if (Input.GetKeyDown("2"))
+		{
+			cs.SetCam2();	
+		}
+        //from solidimporter
+        if(Input.GetKeyDown("p"))
+        {
+            si.ImportSolid();
+        }
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+
+        float curSpeedX = canMove ? walkingSpeed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? walkingSpeed * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
+
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-         moveDirection.y = movementDirectionY;
-
-
-        // Move the controller
+        moveDirection.y = movementDirectionY;
         characterController.Move(moveDirection * Time.deltaTime);
 
         // Player and Camera rotation
