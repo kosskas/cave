6d13041a106a4c30a1efb1415f368397707d7382
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-20 21:00:17 +0100

    Add primitive projection

--- Assets/Scripts/VertexProjecter.cs
+++ Assets/Scripts/VertexProjecter.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class VertexProjecter : MonoBehaviour {
-
+	// TODO
+	// Rozwiązać problem z MeshColliderem
+	// Dodać ile razy była kolizja
+	// Dodać linie
+	
 	Dictionary<string, Vector3> labeledVertices;
+	GameObject[] points;
 	public void InitVertexProjecter(Dictionary<string, Vector3> labeledVertices){
 		this.labeledVertices = labeledVertices;
-		//GenerateRays();
-	}
-
-	// Use this for initialization
-	void Start () {
-		
+		CreateHitPoints();
 	}
 	
 	// Update is called once per frame
@@ -21,30 +23,32 @@ public class VertexProjecter : MonoBehaviour {
 	}
 	void GenerateRays()
     {
+		int i = 0;
         foreach (var pair in labeledVertices)
         {
 
 			Vector3 vertex = transform.TransformPoint(pair.Value); //magic
  			// Ray w kierunku X
             Ray rayX = new Ray(vertex, Vector3.right*10);
-            Debug.DrawRay(vertex, Vector3.right*10);
-			//DrawRay(rayX);
+            //Debug.DrawRay(vertex, Vector3.right*10);
+			DrawRay(rayX,i);
 
-            // Ray w kierunku Y
+            // Ray w kierunku -Y
             Ray rayY = new Ray(vertex, Vector3.down*10);
-            Debug.DrawRay(vertex, Vector3.down*10);
-			//DrawRay(rayY);
+            //Debug.DrawRay(vertex, Vector3.down*10);
+			DrawRay(rayY,i+1);
 
             // Ray w kierunku Z
             Ray rayZ = new Ray(vertex, Vector3.forward*10);
-            Debug.DrawRay(vertex, Vector3.forward*10);
-			//DrawRay(rayZ);
+            //Debug.DrawRay(vertex, Vector3.forward*10);
+			DrawRay(rayZ,i+2);
+			i+=3;
         }
     }
-    void DrawRay(Ray ray)
+    void DrawRay(Ray ray, int idx)
     {
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit) && hit.collider.tag != "Solid")
         {
             // Rysuj linię reprezentującą Ray
 			/*
@@ -55,13 +59,18 @@ public class VertexProjecter : MonoBehaviour {
 		lineRenderer.SetPosition(1, hit.point);
 
 		line.transform.SetParent(gameObject.transform);
-		*/
-            // Dodaj punkt w miejscu przecięcia
-            GameObject point = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            point.transform.position = hit.point;
-            point.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-            Destroy(point.GetComponent<Collider>()); // Usuń collider, aby uniknąć kolizji z kolejnymi rayami
-			point.transform.SetParent(gameObject.transform);
+		*/           
+            points[idx].transform.position = hit.point;
         }
+    }
+	private void CreateHitPoints()
+    {
+		points = new GameObject[3* labeledVertices.ToArray().Length];
+        for (int i =0; i < points.Length; i++){
+			points[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			points[i].transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+			Destroy(points[i].GetComponent<Collider>());
+			points[i].transform.SetParent(gameObject.transform);
+		}
     }
 }
