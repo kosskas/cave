19d94816f9cfdce3c3f1e8f2d58d1f82d1ed7aa0
kosskas <93159483+kosskas@users.noreply.github.com>
2024-01-20 18:56:42 +0100

    Add VertexProjecter script

---
+++ Assets/Scripts/VertexProjecter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexProjecter : MonoBehaviour {
+
+	void InitVertexProjecter(){
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
