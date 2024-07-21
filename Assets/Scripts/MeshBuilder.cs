76cd37eb08c8b29da1fa5b5f7169ae6611639610
kosskas <93159483+kosskas@users.noreply.github.com>
2024-07-21 19:49:50 +0200

    Add more scenarios

--- Assets/Scripts/MeshBuilder.cs
+++ Assets/Scripts/MeshBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Threading;
 
 public class MeshBuilder : MonoBehaviour {
 	
@@ -12,14 +13,20 @@ public class MeshBuilder : MonoBehaviour {
 		public PointProjection(GameObject pointObject)
 		{
 			this.pointObject = pointObject;
-		}
+        }
 
-	}
-	
+    }
+    private enum Status {
+        OK,
+        PLANE_ERR,
+        OTHER_ERR
+    }
+
+
     /// <summary>
     /// Opisuje zbiór rzutowanych wierzchołków na danej płaszczyźnie
     /// </summary>
-	/// 
+    /// 
 
     Dictionary<WallInfo, Dictionary<string, GameObject>> verticesOnWalls;
 	Dictionary<string, Point> vertices3D;
@@ -81,13 +88,43 @@ public class MeshBuilder : MonoBehaviour {
 			verticesOnWalls[wall] = new Dictionary<string, GameObject>();
         }
 		//sprawdz czy istnieja już dwa
-		verticesOnWalls[wall][label] = pointObject;
+		List<GameObject> currPts = GetCurrentPointProjections(label);
 
-		Debug.Log($"Point added: wall[{wall.number}] label[{label}] N={wall.GetNormal()} ---- {pointObject.transform.position}");
+		if(currPts.Count < 1)
+		{
+            //nie bylo zadnych rzutow tego pktu
+            //bedzie 1
+            verticesOnWalls[wall][label] = pointObject;
+        }
+        if (currPts.Count < 2)
+        {
+            //jest juz 1 bedzie 2
+            //podejmij rekonstrukcje
+            //sprawdz plawszczyzny
+            Status result = ReconstructPoint(label);
+        }
+        else
+		{
+			//sa juz 2
+			//sprawdz czy dobrze postawiony trzeci
+            Vector3 rzut1 = currPts[0].transform.position;
+            Vector3 rzut2 = currPts[1].transform.position;
 
-		//* Gdy 2 o tej samej etykiecie, na innych ścianach, sprawdź czy można postawić w 3D 
-		//sprawdz czy juz są dwa - postaw 3 z automatu?
-		CheckReconstruction(label);
+			Vector3 rzut3 = pointObject.transform.position;
+
+            Vector3 test1 = RecreatePoint(rzut1, rzut2);
+            Vector3 test2 = RecreatePoint(rzut2, rzut3);
+            Vector3 test3 = RecreatePoint(rzut1, rzut3);
+
+
+
+        }
+        Debug.Log($"Point added: wall[{wall.number}] label[{label}] N={wall.GetNormal()} ---- {pointObject.transform.position}");
+
+        //* Gdy 2 o tej samej etykiecie, na innych ścianach, sprawdź czy można postawić w 3D 
+        //sprawdz czy juz są dwa - postaw 3 z automatu?
+
+
 
     }
 
@@ -102,26 +139,23 @@ public class MeshBuilder : MonoBehaviour {
 			}
 		}
 		//usun takze 3D
+        //jezeli sa 3, usuwasz 1, sprawdz nowa pozycje 3d
 	}
 
-	private void CheckReconstruction(string label)
+	private Status ReconstructPoint(string label)
 	{
 		int licz = 0;
 
-        GameObject[] pointsInfo = new GameObject[MAXWALLSNUM];
-
-		foreach (WallInfo wall in verticesOnWalls.Keys)
-		{
-			if (verticesOnWalls[wall].ContainsKey(label))
-			{
-				pointsInfo[licz++] = verticesOnWalls[wall][label];
-			}
-		}
+        List<GameObject> pointsInfo = GetCurrentPointProjections(label);
+        //sprawdz ile jest juz rzutow jednego pktu
+        licz = pointsInfo.Count;
+		///ja nie wiem w jakiej kolejności to wypluwa rzuty. dopoki sa 2 to ok
 		if(licz == 2)
 		{
 			Vector3 rzut1 = pointsInfo[0].transform.position;
 			Vector3 rzut2 = pointsInfo[1].transform.position;
-			Vector3 pkt3D = ReconstructPoint(rzut1, rzut2);
+
+			Vector3 pkt3D = RecreatePoint(rzut1, rzut2);
 			if (pkt3D != Vector3.zero)
 			{
                 GameObject obj = new GameObject("Vertex3D " + label);
@@ -133,13 +167,14 @@ public class MeshBuilder : MonoBehaviour {
                 vertexObject.SetLabel(label, VERTEX_LABEL_SIZE, Color.white);
 
                 vertices3D[label] = vertexObject;
+                return Status.OK;
 
             }
 			else
 			{
 				//podswietl je na czerwono
                 Debug.Log($"Rzuty pktu nie leza na jednej plaszczyznie");
-
+                return Status.PLANE_ERR;
 
             }
 
@@ -150,23 +185,22 @@ public class MeshBuilder : MonoBehaviour {
             //Problem
 			//Sprawdz czy 3 jest dobrze polozony
             Debug.Log($"Jest więcej rzutow pktu niz 2");
+            return Status.OTHER_ERR;
+
         }
+        return Status.OTHER_ERR;
 
-	}
-    private Vector3 ReconstructPoint(Vector3 vec1, Vector3 vec2)
+    }
+    private Vector3 RecreatePoint(Vector3 vec1, Vector3 vec2)
     {
-
-        /*
-		v1 = (A, b, c)
-		v2 = (A, e, c)
-		v3 = (A, b, f)
-		ret = (A, e, f)
-		*/
         Vector3 ret = Vector3.zero;
         const float eps = 0.0001f;
-        bool cmp_x = Mathf.Abs(vec1[0] - vec2[0]) < eps;
-        bool cmp_y = Mathf.Abs(vec1[1] - vec2[1]) < eps;
-        bool cmp_z = Mathf.Abs(vec1[2] - vec2[2]) < eps;
+		const float C = 1000;
+		
+
+		bool cmp_x = Mathf.Abs(vec1.x - vec2.x) < eps;
+        bool cmp_y = Mathf.Abs(vec1.y - vec2.y) < eps;
+        bool cmp_z = Mathf.Abs(vec1.z - vec2.z) < eps;
 
 		Debug.Log($"{cmp_x}{cmp_y}{cmp_z}");
 		if(!(cmp_x || cmp_y || cmp_z))
@@ -176,23 +210,37 @@ public class MeshBuilder : MonoBehaviour {
 
 		if (cmp_x)
 		{
-			ret.x = vec1.x;
-			ret.y = Mathf.Max(vec1.y,vec2.y);
-            ret.z = Mathf.Max(vec1.z, vec2.z);
+			ret.x = Mathf.Floor(vec1.x * C) / C;
+			ret.y = Mathf.Floor(Mathf.Max(vec1.y, vec2.y) * C) / C;
+            ret.z = Mathf.Floor(Mathf.Max(vec1.z, vec2.z) * C) / C;
         }
         if (cmp_y)
         {
-            ret.x = Mathf.Min(vec1.x,vec2.x);
-            ret.y = vec1.y;
-            ret.z = Mathf.Max(vec1.z, vec2.z);
+            ret.x = Mathf.Floor(Mathf.Min(vec1.x, vec2.x) * C) / C;
+            ret.y = Mathf.Floor(vec1.y * C) / C;
+            ret.z = Mathf.Floor(Mathf.Max(vec1.z, vec2.z) * C) / C;
         }
         if (cmp_z)
         {
-            ret.x = Mathf.Min(vec1.x, vec2.x);
-            ret.y = Mathf.Max(vec1.y, vec2.y);
-            ret.z = vec2.z;
+            ret.x = Mathf.Floor(Mathf.Min(vec1.x, vec2.x) * C) / C;
+            ret.y = Mathf.Floor(Mathf.Max(vec1.y, vec2.y) * C) / C;
+            ret.z = Mathf.Floor(vec2.z * C) / C;
         }
         ///Jeżeli rzuty nie znajdują sie na jednej płasz. to DO STH
         return ret;
+    }
+
+	private List<GameObject> GetCurrentPointProjections(string label)
+	{
+		List<GameObject> pointsInfo = new List<GameObject>();
+        //sprawdz ile jest juz rzutow jednego pktu
+        foreach (WallInfo wall in verticesOnWalls.Keys)
+        {
+            if (verticesOnWalls[wall].ContainsKey(label))
+            {
+                pointsInfo.Add(verticesOnWalls[wall][label]);
+            }
+        }
+		return pointsInfo;
     }
 }
