55734b84e112376729e7a80704b84686e44e7ed6
kosskas <93159483+kosskas@users.noreply.github.com>
2024-08-24 15:10:18 +0200

    Add SolidExporter static class template

---
+++ Assets/Scripts/SolidExporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SolidExporter{
+    /// <summary>
+    /// Ścieżka względna dostępu do katalogu zawierającego pliki w formacie .wobj
+    /// </summary>
+    #if UNITY_EDITOR
+        private const string pathToFolderWithSolids = "./Assets/Figures3D";
+    #else
+        private const string pathToFolderWithSolids = "./Figures3D";
+    #endif
+
+    /// <summary>
+    /// Rozszerzenie plików zawierających opis cystomowych brył
+    /// </summary>
+    private const string solidFileExt = "*.wobj";
+}
