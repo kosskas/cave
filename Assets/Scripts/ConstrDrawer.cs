afdfe0c462490cad3c63cf873edfe39b09bc2346
kosskas <93159483+kosskas@users.noreply.github.com>
2024-10-25 18:59:43 +0200

    Add construction drawer class

---
+++ Assets/Scripts/ConstrDrawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstrDrawer : MonoBehaviour
+{
+    private bool showCircles = true;
+
+    private bool showLines = true;
+
+	GameObject constrDrawingsDir = null;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Clear()
+    {
+
+    }
+
+    public void CreateHelpingLine(Vector3 pos1, Vector3 pos2, WallInfo wall)
+    {
+
+    }
+    public void CreateHelpingCircle(Vector3 pos1, Vector3 pos2, WallInfo wall)
+    {
+
+    }
+}
