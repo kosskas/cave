148b64495fbd7b29e4e1bce8885fba5064e3555f
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2024-10-29 15:03:30 +0100

    Add player following and overlay rendering

--- Assets/Scripts/Experimental/IndexedLabel.cs
+++ Assets/Scripts/Experimental/IndexedLabel.cs
@@ -11,26 +11,30 @@ namespace Assets.Scripts.Experimental
 {
     public class IndexedLabel : MonoBehaviour
     {
-        private Vector3 Offset(Transform t) => 0.08f * _fontSize * t.up + 0.05f * t.right;
-
-
-        private string _text = "";
-        private string _upperIndex = "";
-        private string _lowerIndex = "";
-
-        private float _fontSize = 1;
+        private Vector3 Offset(Transform t) => 0.08f * FontSize * t.up;
 
         private TextMeshPro _textMeshPro;
 
-        private Vector3 _pos;
+        private GameObject _player;
+
+        public string Text { get; set; } = "";
+
+        public string UpperIndex { get; set; } = "";
+
+        public string LowerIndex { get; set; } = "";
+
+        public float FontSize { get; set; } = 1;
 
 
         void Start()
         {
+            _player = GameObject.Find("FPSPlayer");
+
             GameObject textObj = new GameObject("LABEL");
             textObj.transform.SetParent(transform);
 
             _textMeshPro = textObj.AddComponent<TextMeshPro>();
+            _textMeshPro.fontMaterial.shader = Shader.Find("TextMeshPro/Distance Field Overlay");
             _textMeshPro.color = Color.black;
             _textMeshPro.alignment = TextAlignmentOptions.Center;
         }
@@ -38,62 +42,11 @@ namespace Assets.Scripts.Experimental
         void Update()
         {
             _textMeshPro.transform.position = gameObject.transform.position + Offset(gameObject.transform);
-            _textMeshPro.fontSize = _fontSize;
-            _textMeshPro.text = String.IsNullOrWhiteSpace(_text) ? "" : $"{_text}<sup>{_upperIndex}</sup><sub>{_lowerIndex}</sub>";
-        }
+            _textMeshPro.fontSize = FontSize;
+            _textMeshPro.text = String.IsNullOrWhiteSpace(Text) ? "" : $"{Text}<sup>{UpperIndex}</sup><sub>{LowerIndex}</sub>";
 
-
-        // Properties
-        public string Text
-        {
-            get
-            {
-                return _text;
-            }
-
-            set
-            {
-                _text = value;
-            }
-        }
-
-        public string UpperIndex
-        {
-            get
-            {
-                return _upperIndex;
-            }
-
-            set
-            {
-                _upperIndex = value;
-            }
-        }
-
-        public string LowerIndex
-        {
-            get
-            {
-                return _lowerIndex;
-            }
-
-            set
-            {
-                _lowerIndex = value;
-            }
-        }
-
-        public float FontSize
-        {
-            get
-            {
-                return _fontSize;
-            }
-
-            set
-            {
-                _fontSize = value;
-            }
+            Vector3 directionToPlayer = _player.transform.position - _textMeshPro.transform.position;
+            _textMeshPro.transform.rotation = Quaternion.LookRotation(-directionToPlayer);
         }
     }
 }
