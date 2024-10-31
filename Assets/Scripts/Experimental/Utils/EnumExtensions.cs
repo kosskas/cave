035613714c3cec71a62bd162af5c4953ec27ee80
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2024-10-31 01:05:49 +0100

    Add enum description extension

---
+++ Assets/Scripts/Experimental/Utils/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Experimental.Utils
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
