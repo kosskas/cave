dfb08b59e76c954af7f804fd1ca6831b04402af1
OkK33 <oruczynski@interia.pl>
2024-11-12 13:10:05 +0100

    Forgot to commit files

---
+++ Assets/Scripts/Walls/FaceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceInfo {
+
+    
+    public List<KeyValuePair<string, Vector3>> Points { get; set; }
+    public GameObject FaceObject { get; set; }
+
+    public FaceInfo(List<KeyValuePair<string, Vector3>> points, GameObject faceObject)
+    {
+        Points = points;
+        FaceObject = faceObject;
+    }
+}
